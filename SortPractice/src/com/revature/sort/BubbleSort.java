9ad388e21c72040f5989f7524ebb7f547760a89b
gao-shan <sam.mosca@outlook.com>
2019-11-05 11:57:06 -0500

    first attempt SJ

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -4,5 +4,13 @@ public class BubbleSort {
 	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < arr.length; i++) {
+			int tempStart = arr[i];
+			int n = (arr.length - 1) - i;
+			int tempEnd = arr[n];
+			if (tempEnd < tempStart) {
+				tempEnd = tempStart;
+			}
+		}
 	}
 }
