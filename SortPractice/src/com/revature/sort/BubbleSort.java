bccbe76393c735c95873fed31c1ea14c7db3249d
Jeffreychangg <57107988+Jeffreychangg@users.noreply.github.com>
2019-11-05 16:15:56 -0500

    Update BubbleSort.java

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -4,13 +4,18 @@ public class BubbleSort {
 	
 	public static void sort(int[] arr) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < arr.length; i++) {
-			int tempStart = arr[i];
-			int n = (arr.length - 1) - i;
-			int tempEnd = arr[n];
-			if (tempEnd < tempStart) {
-				tempEnd = tempStart;
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=arr.length-1;j>i;j--) {
+				
+				if(arr[i]>arr[j]) {
+					int temp;
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+					}
+				
 			}
+			
 		}
 	}
 }
